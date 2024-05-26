4abee432a59e443dd4d972eae97da51fb6b266e0
juanB <juancruzberrios73@gmail.com>
2024-05-26 17:01:21 -0300

    first commit

---
+++ src/main/java/com/DulceAromaGrupo8/DulceAromaGrupo8/DulceAromaGrupo8Application.java
@@ -0,0 +1,13 @@
+package com.DulceAromaGrupo8.DulceAromaGrupo8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DulceAromaGrupo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DulceAromaGrupo8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/DulceAromaGrupo8/DulceAromaGrupo8/DulceAromaGrupo8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DulceAromaGrupo8.DulceAromaGrupo8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DulceAromaGrupo8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
